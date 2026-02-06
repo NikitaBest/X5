782e9e6977d1c9e771f6f86162184dc63b379d88
NikitaBest <bestbnikita@gmail.com>
2026-02-06 19:55:27 +0100

    убраны поля для заголовоках

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,11 @@
 import BackButton from '../ui/BackButton.jsx'
-import GlassCard from '../ui/GlassCard.jsx'
 import './Header.css'
 
 function Header({ title, showBack = true }) {
   return (
     <header className="page-header">
       {showBack && <BackButton />}
-      <GlassCard className="header-glass-card">
-        <h1 className="header-title">{title}</h1>
-      </GlassCard>
+      <h1 className="header-title">{title}</h1>
     </header>
   )
 }
