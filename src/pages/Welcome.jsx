0fbdc89bf0b897e1651c5502bc189f078f2427b8
NikitaBest <bestbnikita@gmail.com>
2026-03-20 16:06:39 +0300

    уведомление на странице целей

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -140,9 +140,7 @@ function Welcome() {
 
         {hasValidGoals && (
           <div className="welcome-success-message">
-            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M16.6667 5L7.50004 14.1667L3.33337 10" stroke="#5DAF2E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
+            <img src="/cel.svg" alt="" className="welcome-success-icon" aria-hidden="true" />
             <span>
               Отлично! Сфокусируемся на {selectedCount === 1 ? 'этой цели' : 'этих двух целях'}.
             </span>
